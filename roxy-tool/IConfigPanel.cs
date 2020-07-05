8ed0c67e54a93b61ca4d66f0930b09adf15ad241
veroxzik <43590004+veroxzik@users.noreply.github.com>
2020-07-04 21:10:54 -0400

    Initial commit

---
+++ roxy-tool/IConfigPanel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace roxy_tool
+{
+    interface IConfigPanel
+    {
+        void PopulateControls(byte[] configBytes);
+
+        byte[] GetConfigBytes();
+
+        void PopulateRgbControls(byte[] configBytes);
+
+        byte[] GetRgbConfigBytes();
+
+        int GetComboBoxIndex(sbyte sens);
+
+        sbyte GetByteFromComboBox(int index);
+    }
+}
---
+++ roxy-tool/OptionStrings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace roxy_tool
+{
+    public static class OptionStrings
+    {
+        public static readonly string BoardLabel = "boardLabel";
+        public static readonly string HideSerial = "hideSerial";
+        public static readonly string InvertQe1 = "invertQe1";
+        public static readonly string InvertQe2 = "invertQe2";
+        public static readonly string Led1On = "led1On";
+        public static readonly string Led2On = "led2On";
+        public static readonly string AnalogInput = "analogInput";
+        public static readonly string AnalogButtons = "analogButtons";
+        public static readonly string Qe1Sensitivity = "qe1Combo";
+        public static readonly string Qe2Sensitivity = "qe2Combo";
+        public static readonly string Ps2Mode = "ps2Combo";
+        public static readonly string RgbInterface = "rgbInterface";
+        public static readonly string RgbBrightness = "rgbBrightness";
+        public static readonly string DebounceTime = "debounceTime";
+        public static readonly string RgbMode = "rgbMode";
+        public static readonly string Rgb1Color = "rgb1Color";
+        public static readonly string Rgb2Color = "rgb2Color";
+        public static readonly string AscEmulation = "ascEmulation";
+    }
+}
